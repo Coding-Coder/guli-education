63239ec75db5599929131f069f247d5ccf32bbdd
lxy <aethon47@163.com>
2021-06-17 09:42:44 +0800

    feat:完善课程详情立即购买|立即观看按钮

--- service/service-edu/src/main/java/com/lxy/eduservice/controller/front/CourseFrontController.java
+++ service/service-edu/src/main/java/com/lxy/eduservice/controller/front/CourseFrontController.java
@@ -4,6 +4,8 @@ package com.lxy.eduservice.controller.front;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lxy.commonutils.R;
 import com.lxy.commonutils.ordervo.CourseWebVoOrder;
+import com.lxy.commonutils.utils.JwtUtils;
+import com.lxy.eduservice.client.OrdersClient;
 import com.lxy.eduservice.entity.EduCourse;
 import com.lxy.eduservice.entity.chapter.ChapterVo;
 import com.lxy.eduservice.entity.frontvo.CourseFrontVo;
@@ -15,6 +17,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +32,9 @@ public class CourseFrontController {
     @Autowired
     private EduChapterService chapterService;
 
+    @Autowired
+    private OrdersClient ordersClient;
+
     @ApiOperation("课程列表(条件查询带分页)")
     @PostMapping("getFrontCourseList/{page}/{limit}")
     public R getFrontCourseList(@PathVariable long page,
@@ -41,14 +47,14 @@ public class CourseFrontController {
 
     @ApiOperation("查询课程详情")
     @GetMapping("getFrontCourseInfo/{courseId}")
-    public R getFrontCourseInfo(@PathVariable String courseId) {
+    public R getFrontCourseInfo(@PathVariable String courseId, HttpServletRequest request) {
         //根据课程id，编写sql语句查询课程信息
         CourseWebVo courseWebVo = courseService.getBaseCourseInfo(courseId);
-
         //根据课程id查询章节和小节
         List<ChapterVo> chapterVideoList = chapterService.getChapterVideoByCourseId(courseId);
-
-        return R.ok().data("courseWebVo", courseWebVo).data("chapterVideoList", chapterVideoList);
+        //根据课程id和用户id查询当前课程是否已经支付过了
+        boolean isBuyCourse = ordersClient.isBuyCourse(courseId, JwtUtils.getMemberIdByJwtToken(request));
+        return R.ok().data("courseWebVo", courseWebVo).data("chapterVideoList", chapterVideoList).data("isBuy", isBuyCourse);
     }
 
     @ApiOperation("根据课程id查询课程信息")
--- service/service-order/src/main/java/com/lxy/order/controller/OrderController.java
+++ service/service-order/src/main/java/com/lxy/order/controller/OrderController.java
@@ -45,5 +45,17 @@ public class OrderController {
         Order order = orderService.getOne(wrapper);
         return R.ok().data("item", order);
     }
+
+    @ApiOperation("根据课程id和用户id查询订单表中订单状态")
+    @GetMapping("isBuyCourse/{courseId}/{memberId}")
+    public boolean isBuyCourse(@PathVariable String courseId,
+                               @PathVariable String memberId) {
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.eq("course_id", courseId);
+        wrapper.eq("member_id", memberId);
+        wrapper.eq("status", 1);//支付状态 1代表已经支付
+        int count = orderService.count(wrapper);
+        return count > 0;//已经支付
+    }
 }
 
